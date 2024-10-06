1d56948d658bf5e7b57f66bc49fde417947dc420
Savio Shaji <savioshaji02@gmail.com>
2024-10-06 11:37:53 +0530

    add toggleComplete

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import { useState } from "react"
 
 function App() {
 
-const [tasks,setTasks] = useState([])
+const [tasks,setTasks] = useState([{description : 'gdhdhd',completed:true }])
 const [newtask,setNewTask] = useState()
   
 const addTask = () => {
   setTasks([
-    ...tasks,newtask
+    ...tasks,{description:newtask,completed:false}
   ])
   setNewTask("")
 }
 
 const deleteTask = (taskTobeDeleted)=>{
   setTasks(tasks.filter(task=>task!== taskTobeDeleted))
+}
+
+const toggleComplete = (taskToBeToggled)=>{
+  setTasks(
+    tasks.map((task)=> task===taskToBeToggled ? {...task , completed : !task.completed} : task )
+  )
 }
 
   return (
@@ -24,8 +30,28 @@ const deleteTask = (taskTobeDeleted)=>{
       <p>All tasks :</p>
       <ul>
         {tasks.map((task,index)=>
-          <li key={index}>{task}<button onClick={()=>deleteTask(task)}>Delete</button></li>
+          <li key={index}>
+            <span style={{textDecoration :task.completed ? 'line-through' : 'none' }}>
+              {task.description}
+            </span>
+            <button onClick={()=>toggleComplete(task)}>
+              ✅
+              </button>
+                <button onClick={()=>deleteTask(task)}>
+                🗑️
+                  </button>
+          </li>
         )}
+      </ul>
+
+      <p>Completed :</p>
+      <ul>
+        {tasks.map((task,index)=> task.completed ? <li key={index}>{task.description}</li> : null )}
+      </ul>
+
+      <p>Not Completed :</p>
+      <ul>
+        
       </ul>
     </>
   )
